920c9f2ad0e6b4f8d0f1b02a7acc35bc42846d71
AlanFontoura <alan.fontoura@d1g1t.com>
2025-04-08 10:03:21 -0400

    Initial DB access code

---
+++ db_access/db_access.py
@@ -0,0 +1,26 @@
+import pyodbc
+import pandas as pd
+
+dsn_name = "gresham_staging"
+
+try:
+    connection = pyodbc.connect(f"DSN={dsn_name}")
+    # cursor = connection.cursor()
+
+    # cursor.execute("SELECT * FROM fees_feeschedule;")
+    # rows = cursor.fetchall()
+    
+    # for row in rows:
+        # print(row)
+        
+    query = "SELECT * FROM fees_feeschedule;"
+    df = pd.read_sql(query, connection)
+    print(df)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    # if cursor:
+        # cursor.close()
+    if connection:
+        connection.close()
