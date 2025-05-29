564596e9470b4c58d7ae7c5f5feefaa4e0481a12
AlanFontoura <alan.fontoura@d1g1t.com>
2025-05-29 11:54:27 -0400

    Add positions and transactions loading to daily delta

---
+++ my_daily_recon/recon_daily_delta.py
@@ -0,0 +1,141 @@
+import os
+import awswrangler as wr
+import pandas as pd
+from configparser import ConfigParser
+import numpy as np
+import logging
+from base_main import BaseMain
+
+LOG = logging.getLogger(__name__)
+
+
+"""
+
+
+"""
+
+
+class ReconDailyDelta(BaseMain):
+    def __init__(self):
+        super().__init__()
+        constants = ConfigParser()
+        constants.read("my_daily_recon/inputs/my_daily_recon.ini")
+        # self.client = constants.get(self.args.profile, "CLIENT")
+        # self.env = constants.get(self.args.profile, "ENVIRONMENT")
+        # self.region = constants.get(self.args.profile, "REGION")
+        self.custodian_folder = constants.get(self.args.profile, "CUSTODIAN_FOLDER")
+
+    def add_extra_args(self):
+        self.parser.add_argument(
+            "-p",
+            "--profile",
+            dest="profile",
+            type=str,
+            required=True,
+            help="Client profile to be used",
+        )
+
+    def get_recent_recon_files(self):
+        """
+        Get the most recent reconciliation files from S3.
+        """
+        outpuyt_dir = "my_daily_recon/outputs/"
+        files = os.listdir(outpuyt_dir)
+        files = [file for file in files if "full_recon" in file]
+        files.sort()
+        current = pd.read_csv(os.path.join(outpuyt_dir, files[-1]))
+        previous = pd.read_csv(os.path.join(outpuyt_dir, files[-2]))
+        return current, previous
+
+    def filter_recon_file(self, recon_file):
+        """
+        Filter the reconciliation file to include only relevant columns.
+        """
+        recon_file["reconciled"] = (
+            recon_file["Units - Reconciled"] & recon_file["Market Value - Reconciled"]
+        )
+        recon_file = recon_file[~recon_file["reconciled"]]
+        recon_file.drop(columns="reconciled", inplace=True, errors="ignore")
+        return recon_file
+
+    def compare_recon_files(self, current, previous):
+        full_recon = current.merge(
+            previous, on=["Account ID", "Security ID"], indicator=True, how="outer"
+        )
+        delta = full_recon.loc[
+            full_recon["_merge"] == "left_only", ["Account ID", "Security ID"]
+        ]
+        new_breaks = current.merge(delta, how="inner")
+        return new_breaks
+
+    def get_transactions(self, new_breaks):
+        transaction_files = wr.s3.list_objects(
+            self.custodian_folder, suffix="Transaction.csv"
+        )
+        transaction_files = [
+            file for file in transaction_files if file.split("/")[5] > "20250318"
+        ]
+        dfl = []
+        filtering_df = new_breaks[["Account ID", "Security ID"]].rename(
+            columns={"Account ID": "AccountCode", "Security ID": "SecurityID1"}
+        )
+
+        for file in transaction_files:
+            df = wr.s3.read_csv(file)
+            df = df.merge(filtering_df, how="inner")
+            df["source"] = file.split("/")[5]
+            dfl.append(df)
+
+        transactions = (
+            pd.concat(dfl, ignore_index=True)
+            .sort_values(by=["AccountCode", "SecurityID1", "source"])
+            .reset_index(drop=True)
+        )
+        return transactions
+
+    def get_positions(self, new_breaks):
+        position_files = wr.s3.list_objects(
+            self.custodian_folder, suffix="Position.csv"
+        )
+        position_files = [
+            file for file in position_files if file.split("/")[5] > "20250318"
+        ]
+        dfl = []
+        filtering_df = new_breaks[["Account ID", "Security ID"]].rename(
+            columns={"Account ID": "AccountCode", "Security ID": "SecurityID"}
+        )
+
+        for file in position_files:
+            df = wr.s3.read_csv(file)
+            df = df.merge(filtering_df, how="inner")
+            df["source"] = file.split("/")[5]
+            dfl.append(df)
+
+        positions = (
+            pd.concat(dfl, ignore_index=True)
+            .sort_values(by=["AccountCode", "SecurityID", "source"])
+            .reset_index(drop=True)
+        )
+        return positions
+
+    def run(self):
+        current, previous = self.get_recent_recon_files()
+        current = self.filter_recon_file(current)
+        previous = self.filter_recon_file(previous)
+        new_breaks = self.compare_recon_files(current, previous)
+        positions = self.get_positions(new_breaks)
+        transactions = self.get_transactions(new_breaks)
+        today = new_breaks.loc[0, "Date"]
+        new_breaks.to_csv(f"my_daily_recon/outputs/{today}_new_breaks.csv", index=False)
+        positions.to_csv(
+            f"my_daily_recon/outputs/{today}_new_break_positions.csv", index=False
+        )
+        transactions.to_csv(
+            f"my_daily_recon/outputs/{today}_new_break_transactions.csv", index=False
+        )
+
+
+if __name__ == "__main__":
+    recon_daily_delta = ReconDailyDelta()
+    recon_daily_delta.run()
+    LOG.info("Reconciliation daily delta completed successfully.")
